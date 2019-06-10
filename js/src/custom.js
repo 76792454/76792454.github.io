8255cb5777dcea73243262f0c94367ba7cc11267
Wang xin <76792454@qq.com>
2019-06-10 13:27:27 +0800

    Site updated: 2019-06-10 13:27:24

--- js/src/custom.js
+++ js/src/custom.js
@@ -2,3 +2,11 @@
 function randomColor() {
     return "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")";
 }
+/* 轮播背景图片 */
+$(function () {
+    $.backstretch([  
+          "/images/background/saber1.jpg",
+          "/images/background/saber2.jpg",
+          "/images/background/bg1.jpg"
+    ], { duration: 60000, fade: 1500 });  
+});
